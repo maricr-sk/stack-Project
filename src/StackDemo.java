2a7b33d857fa9bef5acdc9a048ed6e5cc337dd63
maricr-sk <85328841+maricr-sk@users.noreply.github.com>
2024-01-04 01:48:54 -0500

    first commit

---
+++ src/StackDemo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class StackDemo {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+
+        List <Integer> list = new ArrayList<>();
+        Queue <Integer> q = new LinkedList<>();
+    }
+}
